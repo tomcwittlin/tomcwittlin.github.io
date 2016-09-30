f6c48e96c9a2ab82f8c116f1b1cc88e60198fd02
tomcwittlin <tom@tommy-boy.co.uk>
2016-09-30 18:51:48 +0100

    Added portfolio

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ $(document).foundation()
 
 $(document).ready(init);
 
+var $window;
+var $fixedMarker;
+var $sidepanel;
+
 function init() {
   $('#mobilemenuopen, #mobilemenuclose').on('touchstart click', function (e) {
     e.preventDefault();
@@ -14,4 +18,23 @@ function init() {
     slidesToScroll: 1,
     dots: true
   });
+
+  $window = $(window);
+  $fixedMarker = $("#sidepanel-marker");
+  $sidepanel = $("#sidepanel");
+
+  if ($fixedMarker.length && $sidepanel.length) {
+    $window.on('scroll', function() {
+      checkScroll();
+    });
+  }
+  checkScroll();
+}
+
+function checkScroll() {
+  if ($window.scrollTop() > $fixedMarker.position().top) {
+    $sidepanel.addClass("fixed-threshold").removeClass("not-fixed").outerWidth($fixedMarker.width());
+  } else {
+    $sidepanel.addClass("not-fixed").removeClass("fixed-threshold").removeAttr('style');
+  }
 }
